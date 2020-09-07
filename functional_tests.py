2c3b1748f1050cdb31a0722386a00e165fbba055
Fernanda Guimarães <fernandaguimaraes28@gmail.com>
2020-09-07 18:43:08 -0300

    Criando projeto Django e subindo o servidor

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,11 @@
-from selenium import webdriver
+#!/usr/bin/env python3
 
-browser = webdriver.Firefox()
+
+from selenium import webdriver
+from selenium.webdriver.firefox.firefox_binary import FirefoxBinary
+
+binary = FirefoxBinary('/usr/lib/firefox/firefox')
+browser = webdriver.Firefox(firefox_binary=binary)
 browser.get('http://localhost:8000')
 
 assert 'Django' in browser.title
